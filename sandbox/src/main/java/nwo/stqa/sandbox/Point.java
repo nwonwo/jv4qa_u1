be95a065451150c2f879ce86164a807dac632a3b
nwonwo <4wildd@gmail.com>
2016-04-21 17:50:26 +0300

    Создал Start и Point Классы повтороно

---
+++ sandbox/src/main/java/nwo/stqa/sandbox/Point.java
@@ -0,0 +1,16 @@
+package nwo.stqa.sandbox;
+
+public class Point {
+
+  public double x;
+  public double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double distance(Point p) {
+    return Math.sqrt(Math.pow(p.x - x, 2) + Math.pow(p.y - y, 2));
+  }
+}
