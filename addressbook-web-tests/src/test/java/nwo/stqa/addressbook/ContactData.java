1e07c747a10bd2644c5f919967fc21b2add406f8
nwonwo <4wildd@gmail.com>
2016-04-23 21:23:56 +0300

    Test ContactCreation is added

---
+++ addressbook-web-tests/src/test/java/nwo/stqa/addressbook/ContactData.java
@@ -0,0 +1,31 @@
+package nwo.stqa.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String lastname;
+  private final String address;
+  private final String mobile;
+
+  public ContactData(String firstname, String lastname, String address, String mobile) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.address = address;
+    this.mobile = mobile;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+}
